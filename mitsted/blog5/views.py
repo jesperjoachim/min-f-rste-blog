a12564b6b3f1163be0e89a90b8ec8edece91f314
jesper <jesperjoachims@gmail.com>
2018-08-24 10:45:09 +0200

    180822

--- mitsted/blog5/views.py
+++ mitsted/blog5/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import  Opslag
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog5/post_list.html', {})
+    opslag = Opslag.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blog5/post_list.html', {'opslag': opslag})
